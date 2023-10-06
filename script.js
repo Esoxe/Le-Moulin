bf467772da32a8688a4171f2bedc500c4354debd
Esoxe <perret.florian26@gmail.com>
2023-10-06 11:31:30 +0200

    sidebar update

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function afficherResultatPanier(totalPanier) {
     if( totalPanier!==0) {
         resultatPanierElement.textContent=totalPanier;
         document.getElementById("compteur").style.visibility = "visible";
+        AffchageUIPanier(produit)
     } else {
         resultatPanierElement.textContent="";
     }
@@ -24,6 +25,9 @@ function DetermineProduit(produit)
 {
     switch (produit) {
         case 1:
+        var img_produit_1 = document.getElementById("img1").value;
+        var nom_produit_1 = document.getElementById("img1").value;
+        var _produit_1 = document.getElementById("img1").value;
         return document.getElementById("quantite1").value;
     
         case 2:
@@ -33,4 +37,15 @@ function DetermineProduit(produit)
             return 0;
     }
 }
+function AffichageUIPanier()
+{
 
+}
+
+function openSidebar() 
+{
+    document.getElementById("mySidebar").style.width = "250px";
+}
+function closeSidebar () {
+    document.getElementById("mySidebar").style.width="0" ;
+}
